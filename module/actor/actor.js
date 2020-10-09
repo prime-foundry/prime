7d9075de3118933e35048b89b9009c9a84eb9a2b
Johnathan Castle <castle.johnathan@googlemail.com>
2020-10-09 22:41:16 +0100

    Reformated to use tabs instead of spaces. Refactored to use new XP stat.

--- module/actor/actor.js
+++ module/actor/actor.js
@@ -2,46 +2,60 @@
  * Extend the base Actor entity by defining a custom roll data structure which is ideal for the Simple system.
  * @extends {Actor}
  */
-export class BoilerplateActor extends Actor {
+export class BoilerplateActor extends Actor
+{
 
-  /**
-   * Augment the basic actor data with additional dynamic data.
-   */
-  prepareData() {
-    super.prepareData();
+	/**
+	 * Augment the basic actor data with additional dynamic data.
+	 */
+	prepareData()
+	{
+		super.prepareData();
 
-    const actorData = this.data;
-    const data = actorData.data;
-    const flags = actorData.flags;
+		const actorData = this.data;
+		const data = actorData.data;
+		const flags = actorData.flags;
 
-    // Make separate methods for each Actor type (character, npc, etc.) to keep
-    // things organized.
-    if (actorData.type === 'character') this._prepareCharacterData(actorData);
-  }
-
-  /**
-   * Prepare Character type specific data
-   */
-  _prepareCharacterData(actorData) {
-    const data = actorData.data;
-
-    // Make modifications to data here. For example:
-
-    var soulCost = 0;
-    // Loop through ability scores, and add their modifiers to our sheet output.
-    for (let [key, prime] of Object.entries(data.primes)) {
-		// Calculate the modifier using d20 rules.
-		// prime.mod = Math.floor((prime.value - 10) / 2);
-		prime.cost = BoilerplateActor.primeCost(prime.value);
-        soulCost += prime.cost;
-		// mod will go but lets see what we get
-        prime.mod = prime.cost;
+		// Make separate methods for each Actor type (character, npc, etc.) to keep
+		// things organized.
+		if (actorData.type === 'character') this._prepareCharacterData(actorData);
 	}
-    data.soul.value = 100 - soulCost;
-  }
 
-  static primeCost(num) {
-    if (num === 0) return 0;
-    return (num * (num + 1))/2;
-  }
+	/**
+	 * Prepare Character type specific data
+	 */
+	_prepareCharacterData(actorData)
+	{
+		const data = actorData.data;
+
+		// Make modifications to data here. For example:
+
+		data.soul.spent = this.getTotalCost(data.primes);
+		data.xp.spent = this.getTotalCost(data.refinements);
+		// Loop through ability scores, and add their modifiers to our sheet output.
+
+		data.soul.current = (data.soul.initial + data.soul.awarded) - data.soul.spent;
+		data.xp.current = (data.xp.initial + data.xp.awarded) - data.xp.spent;
+	}
+
+	getTotalCost(whatItems)
+	{
+		var totalCost = 0;
+		for (let [key, item] of Object.entries(whatItems))
+		{
+			// Calculate the modifier using d20 rules.
+			// prime.mod = Math.floor((prime.value - 10) / 2);
+			item.cost = BoilerplateActor.primeCost(item.value);
+			totalCost += item.cost;
+			// mod will go but lets see what we get
+			item.mod = item.cost;
+		}
+		return totalCost;
+	}
+
+	static primeCost(num)
+	{
+		if (num === 0) return 0;
+		return (num * (num + 1)) / 2;
+	}
 }
