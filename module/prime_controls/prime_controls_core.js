d6b3a28c8f8c5899d218627669112452ee02b8ee
jackmoxley <jackokmoxley@gmail.com>
2020-10-29 16:16:48 +0000

    Added some Stying, sitched to using the token layer, as it made more sense.

--- module/prime_controls/prime_controls_core.js
+++ module/prime_controls/prime_controls_core.js
@@ -2,44 +2,55 @@ import { PRIME_DICE } from "../prime_dice/prime_dice.js";
 
 Hooks.on("getSceneControlButtons", (controls) => {
 
-    controls.push({
-        name: "primeCoreControl",
-        title: "Prime Controls",
-        // layer: "NotesLayer",
+    const isGM = game.user.isGM;
+    const basicControls = controls[0];
+    basicControls.tools.push({
+        name: "prime-roll",
+        title: game.i18n.localize("PRIME.controls_roll_title"),
         icon: "fas fa-dice-d20",
-        activeTool: 'select',
-        tools: [{
-            name: "select",
-            title: "Prime Controls",
-            icon: "fas fa-expand",
-        }],
+        cssClass: "prime-button",
+        button: true,
+        onClick: () => PRIME_DICE.openPrimeDice("primeControl"),
     });
+    basicControls.tools.push({
+        name: "prime-xp",
+        title: game.i18n.localize("PRIME.controls_xp_title"),
+        icon: "fas fa-expand",
+        cssClass: "prime-button",
+        visible: isGM,
+    });
+
 });
+
+// export class PrimeLayer extends CanvasLayer {
+
+// }
 
 
 Hooks.on("renderSceneControls", () => {
-
-    $('li[data-control="primeCoreControl"]')[0].onclick = () => {
-        PRIME_DICE.openPrimeDice("primeControl");
-        // initMap();
-        // minimap_data.minimapapp.render(true);
-        // minimap_data.shown = true;
-        // if (minimap_data.hooked == false) {
-        //     Hooks.on('canvasPan', async function() {
-        //         resSqau();
-        //     });
-        //     Hooks.on('createToken', async function() {
-        //         resSqau();
-        //     });
-        //     Hooks.on('deleteActor', async function() {
-        //         resSqau();
-        //     });
-        //     Hooks.on('deleteToken', async function() {
-        //         resSqau();
-        //     });
-        //     Hooks.on('updateToken', async function() {
-        //         resSqau();
-        //     });
-        // };
-    }
+    $('li[data-tool="prime-roll"].control-tool').addClass("prime-control");
+    $('li[data-tool="prime-xp"].control-tool').addClass("prime-control");
+    // $('li[data-control="primeCoreControl"]')[0].onclick = () => {
+    //     PRIME_DICE.openPrimeDice("primeControl");
+    // initMap();
+    // minimap_data.minimapapp.render(true);
+    // minimap_data.shown = true;
+    // if (minimap_data.hooked == false) {
+    //     Hooks.on('canvasPan', async function() {
+    //         resSqau();
+    //     });
+    //     Hooks.on('createToken', async function() {
+    //         resSqau();
+    //     });
+    //     Hooks.on('deleteActor', async function() {
+    //         resSqau();
+    //     });
+    //     Hooks.on('deleteToken', async function() {
+    //         resSqau();
+    //     });
+    //     Hooks.on('updateToken', async function() {
+    //         resSqau();
+    //     });
+    // };
+    // }
 });
