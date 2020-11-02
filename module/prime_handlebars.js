268ead3ceb0800ad0cc2a5cb3cdce356c2f18c4a
Johnathan Castle <castle.johnathan@googlemail.com>
2020-11-02 17:13:23 +0000

    Added helper to convert HTML into whitespace or remove it.

--- module/prime_handlebars.js
+++ module/prime_handlebars.js
@@ -19,8 +19,19 @@ export class PrimeHandlebarsPartials
 	}
 }
 
-
-
+Handlebars.registerHelper('convertHTMLForTitle', function (html, options)
+{
+	html = html.replace(/<style([\s\S]*?)<\/style>/gi, '');
+	html = html.replace(/<script([\s\S]*?)<\/script>/gi, '');
+	html = html.replace(/<\/div>/ig, '\n');
+	html = html.replace(/<\/li>/ig, '\n');
+	html = html.replace(/<li>/ig, '  *  ');
+	html = html.replace(/<\/ul>/ig, '\n');
+	html = html.replace(/<\/p>/ig, '\n');
+	html = html.replace(/<br\s*[\/]?>/gi, "\n");
+	html = html.replace(/<[^>]+>/ig, '');
+	return html;
+});
 
 Handlebars.registerHelper('isNotLastItem', function (v1, v2, options)
 {
