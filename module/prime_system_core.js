f0dc59030448e8066c2d8b991ffe0d51b1ee7da1
johnc303 <castle.johnathan@googlemail.com>
2020-11-24 09:21:07 +0000

    Removed redundant example code. Added calls to migration code.

--- module/prime_system_core.js
+++ module/prime_system_core.js
@@ -4,104 +4,65 @@ import { PrimePCActorSheet } from "./actor/actor-sheet.js";
 import { PrimeItem as PrimeItem } from "./item/item.js";
 import { PrimeItemSheet } from "./item/item-sheet.js";
 
+import { PrimeSettingsManager } from "./prime_settings.js";
 import { PrimeHandlebarsPartials } from "./prime_handlebars.js";
+import { PrimeDataMigrationManager } from "./migrations/prime_data_migrations_manager.js";
 
-Hooks.once('init', async function() {
+Hooks.once('init', async function ()
+{
 
-  game.prime = {
-    PrimePCActor,
-    PrimeItem,
-    rollItemMacro
-  };
+	game.prime = {
+		PrimePCActor,
+		PrimeItem
+	};
 
 
-  /**
-   * Set an initiative formula for the system
-   * @type {String}
-   */
-  CONFIG.Combat.initiative = {
-    formula: "1d20 + @abilities.dex.mod",
-    decimals: 2
-  };
+	/**
+	 * Set an initiative formula for the system
+	 * @type {String}
+	 */
+	CONFIG.Combat.initiative = {
+		formula: "1d20 + @abilities.dex.mod",
+		decimals: 2
+	};
 
-  // Define custom Entity classes
-  CONFIG.Actor.entityClass = PrimePCActor;
-  CONFIG.Item.entityClass = PrimeItem;
+	// Define custom Entity classes
+	CONFIG.Actor.entityClass = PrimePCActor;
+	CONFIG.Item.entityClass = PrimeItem;
 
-  // Register sheet application classes
-  Actors.unregisterSheet("core", ActorSheet);
-  Actors.registerSheet("prime", PrimePCActorSheet, { makeDefault: true });
-  Items.unregisterSheet("core", ItemSheet);
-  Items.registerSheet("prime", PrimeItemSheet, { makeDefault: true });
+	// Register sheet application classes
+	Actors.unregisterSheet("core", ActorSheet);
+	Actors.registerSheet("prime", PrimePCActorSheet, { makeDefault: true });
+	Items.unregisterSheet("core", ItemSheet);
+	Items.registerSheet("prime", PrimeItemSheet, { makeDefault: true });
 
-  // If you need to add Handlebars helpers, here are a few useful examples:
-  Handlebars.registerHelper('concat', function() {
-    var outStr = '';
-    for (var arg in arguments) {
-      if (typeof arguments[arg] != 'object') {
-        outStr += arguments[arg];
-      }
-    }
-    return outStr;
-  });
 
-  Handlebars.registerHelper('toLowerCase', function(str) {
-    return str.toLowerCase();
-  });
+
+	// If you need to add Handlebars helpers, here are a few useful examples:
+	Handlebars.registerHelper('concat', function ()
+	{
+		var outStr = '';
+		for (var arg in arguments)
+		{
+			if (typeof arguments[arg] != 'object')
+			{
+				outStr += arguments[arg];
+			}
+		}
+		return outStr;
+	});
+
+	Handlebars.registerHelper('toLowerCase', function (str)
+	{
+		return str.toLowerCase();
+	});
 });
 
-Hooks.once("ready", async function() {
-  // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
-  Hooks.on("hotbarDrop", (bar, data, slot) => createBoilerplateMacro(data, slot));
-  await PrimeHandlebarsPartials.loadPartials();
+Hooks.once("ready", async function ()
+{
+
+	await PrimeSettingsManager.addSettings();
+	await PrimeHandlebarsPartials.loadPartials();
+	await PrimeDataMigrationManager.performIfMigrationRequired();
+
 });
-
-/* -------------------------------------------- */
-/*  Hotbar Macros                               */
-/* -------------------------------------------- */
-
-/**
- * Create a Macro from an Item drop.
- * Get an existing item macro if one exists, otherwise create a new one.
- * @param {Object} data     The dropped data
- * @param {number} slot     The hotbar slot to use
- * @returns {Promise}
- */
-async function createBoilerplateMacro(data, slot) {
-  if (data.type !== "Item") return;
-  if (!("data" in data)) return ui.notifications.warn("You can only create macro buttons for owned Items");
-  const item = data.data;
-
-  // Create the macro command
-  const command = `game.prime.rollItemMacro("${item.name}");`;
-  let macro = game.macros.entities.find(m => (m.name === item.name) && (m.command === command));
-  if (!macro) {
-    macro = await Macro.create({
-      name: item.name,
-      type: "script",
-      img: item.img,
-      command: command,
-      flags: { "prime.itemMacro": true }
-    });
-  }
-  game.user.assignHotbarMacro(macro, slot);
-  return false;
-}
-
-/**
- * Create a Macro from an Item drop.
- * Get an existing item macro if one exists, otherwise create a new one.
- * @param {string} itemName
- * @return {Promise}
- */
-function rollItemMacro(itemName) {
-  const speaker = ChatMessage.getSpeaker();
-  let actor;
-  if (speaker.token) actor = game.actors.tokens[speaker.token];
-  if (!actor) actor = game.actors.get(speaker.actor);
-  const item = actor ? actor.items.find(i => i.name === itemName) : null;
-  if (!item) return ui.notifications.warn(`Your controlled Actor does not have an item named ${itemName}`);
-
-  // Trigger the item roll
-  return item.roll();
-}
