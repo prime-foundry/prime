6988090c36c59e984b0cc4cc9f99bb286e295dcd
johnc303 <castle.johnathan@googlemail.com>
2020-12-01 18:11:16 +0000

    Addressed issues with mousedown event binding potentially closing popup and stealing delete button events. Fixed issues with popups triggering when items are dragging. Adjusted timer to be timeout rather than interval. Added catches to mitigate the issues around the whole sheet disappearing and reappearing due to updates. Commented out debug logs.

--- module/item/item_card_ui.js
+++ module/item/item_card_ui.js
@@ -9,21 +9,30 @@ export class ItemCardUI
 	static bindEvents(whatContainer)
 	{
 		var itemCards = whatContainer.find(".itemCard");
-
-		itemCards.hover(this.cardOver.bind(this), this.cardOut.bind(this))
+		itemCards.hover(this.cardOver.bind(this), this.cardOut.bind(this));
+		itemCards.mousedown(this.testCardClose.bind(this));
 	}
 
 	static cardOver(event)
 	{
 		this.currCard = $(event.delegateTarget);
 
-		this.currCard.overStart = new Date().getTime();
-		this.currCard.timer = setInterval(this.cardOverCounter.bind(this), this.timerInterval);
+		if (this.currCard.hasClass("itemDragging"))
+		{
+			return;
+		}
 
+		this.currCard.overStart = new Date().getTime();
+		this.currCard.timer = setTimeout(this.cardOverCounter.bind(this), this.timerInterval);
 	}
 
 	static cardOverCounter()
 	{
+		if (!this.currCard)
+		{
+			return;
+		}
+
 		const timeDiff = new Date().getTime() - this.currCard.overStart;
 		if (timeDiff >= this.intentDuration)
 		{
@@ -63,11 +72,15 @@ export class ItemCardUI
 			clearInterval(this.currCard.timer);
 			this.currCard.timer = null;
 		}
+		else
+		{
+			this.currCard.timer = setTimeout(this.cardOverCounter.bind(this), this.timerInterval);
+		}
 	}
 
 	static canPopDown(whatCard, whatDescriptionHeight)
 	{
-		console.log("canPopDown(), whatDetailHeight", whatDescriptionHeight);
+		//console.log("canPopDown(), whatDetailHeight", whatDescriptionHeight);
 		var popDownData = {shouldPopDown: true, heightOverRide: null};
 		
 		//alert("Patch this function so it understands about the containers.")
@@ -83,43 +96,43 @@ export class ItemCardUI
 		// We can fit below without increasing any parent heights
 		if (parentHeightOverspill < 0)
 		{
-			console.log("We can fit below without increasing any parent heights");
+			//console.log("We can fit below without increasing any parent heights");
 			return popDownData;
 		}
 
 		// We would cause the grandparent to scroll if we popped down
 		if (grandparentHeightOverspill > 0)
 		{
-			console.log("We would cause the grandparent to scroll if we popped down");
+			//console.log("We would cause the grandparent to scroll if we popped down");
 			const topOverflowAmount = (whatDescriptionHeight - cardTopPos);
 			
 			// If we won't overflow out of the top, the just pop up
 			if (topOverflowAmount <= 0)
 			{
-				console.log("If we won't overflow out of the top, the just pop up. topOverflowAmount: ", topOverflowAmount);
+				//console.log("If we won't overflow out of the top, the just pop up. topOverflowAmount: ", topOverflowAmount);
 				popDownData.shouldPopDown = false;
 				return popDownData;
 			}
 			else // We will overflow out of the top and bottom, so work out which will happen least and cap the height of the detail
 			{
-				console.log("We will overflow out of the top and bottom, so work out which will happen least and cap the height of the detail. topOverflowAmount, grandparentHeightOverspill", topOverflowAmount, grandparentHeightOverspill);
+				//console.log("We will overflow out of the top and bottom, so work out which will happen least and cap the height of the detail. topOverflowAmount, grandparentHeightOverspill", topOverflowAmount, grandparentHeightOverspill);
 				if (topOverflowAmount < grandparentHeightOverspill)
 				{
 					popDownData.heightOverRide = whatDescriptionHeight - topOverflowAmount - 10;
 					popDownData.shouldPopDown = false;
-					console.log("Top overspill is less, popping up but shrinking height to: " + popDownData.heightOverRide);
+					//console.log("Top overspill is less, popping up but shrinking height to: " + popDownData.heightOverRide);
 					return popDownData;
 				}
 				else
 				{
 					popDownData.heightOverRide = whatDescriptionHeight - grandparentHeightOverspill - 10;
-					console.log("Top overspill is more, popping down but shrinking height to: " + popDownData.heightOverRide);
+					//console.log("Top overspill is more, popping down but shrinking height to: " + popDownData.heightOverRide);
 					return popDownData;
 				}
 			}
 		}
 
-		console.log("We might increase parent height but won't grandparent scrolling");
+		//console.log("We might increase parent height but won't grandparent scrolling");
 		return popDownData;
 	}
 
@@ -131,11 +144,23 @@ export class ItemCardUI
 		cardDetail.css({ top: "auto", bottom: "auto", height: 0, "overflow-y": "hidden"});
 		cardDetail.removeClass("popDown popUp");
 		
-		if (this.currCard.timer)
+		if (this.currCard && this.currCard.timer)
 		{
-			clearInterval(this.currCard.timer);
+			clearTimeout(this.currCard.timer);
 		}
 
 		this.currCard = null;
+	}
+
+	static testCardClose(event)
+	{
+		if ($(event.target).hasClass("stopClose"))
+		{
+			return;
+		}
+		else
+		{
+			this.cardOut(event);
+		}
 	}
 }
