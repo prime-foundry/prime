4908847dc72c2031188b9b8ed2a14ef00e0762ac
johnc303 <castle.johnathan@googlemail.com>
2021-01-12 19:07:13 +0000

    Updated .zips and versions.

--- module/migrations/prime_data_migrations_manager.js
+++ module/migrations/prime_data_migrations_manager.js
@@ -1,5 +1,5 @@
 import { PrimeMigration_0_1_7_to_0_1_10 } from "./PrimeMigration_0_1_7_to_0_1_10.js";
-import { PrimeMigration_0_1_10_to_0_1_16 } from "./PrimeMigration_0_1_10_to_0_1_16.js";
+import { PrimeMigration_0_1_10_to_0_1_17 } from "./PrimeMigration_0_1_10_to_0_1_17.js";
 
 export class PrimeDataMigrationManager
 {
@@ -66,7 +66,8 @@ export class PrimeDataMigrationManager
 			case "0.1.13":
 			case "0.1.14":
 			case "0.1.15":
-				PrimeMigration_0_1_10_to_0_1_16.update();
+			case "0.1.16":
+				PrimeMigration_0_1_10_to_0_1_17.update();
 			break;
 			default:
 				const errorMessage = "ERROR: Attempting to migrate from world version '" + currentWorldVersion + "' to system version '" + systemVersion + "' but unable to find matching migration.";
