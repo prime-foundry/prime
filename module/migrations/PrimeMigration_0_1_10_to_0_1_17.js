8928390447dc9d80f14e1654ce1bf02b13280f1f
johnc303 <castle.johnathan@googlemail.com>
2021-01-12 19:09:29 +0000

    Updated class names

--- module/migrations/PrimeMigration_0_1_10_to_0_1_17.js
+++ module/migrations/PrimeMigration_0_1_10_to_0_1_17.js
@@ -1,13 +1,13 @@
-export class PrimeMigration_0_1_10_to_0_1_16
+export class PrimeMigration_0_1_10_to_0_1_17
 {
 	static async update()
 	{
-		ui.notifications.info("Migrating world from 0.1.10 to version 0.1.16.");
+		ui.notifications.info("Migrating world from 0.1.10 to version 0.1.17.");
 		var success = await this.updateItems()
 
 		if (success)
 		{
-			const message = "Migration to version 0.1.16 successful, goats were herded."
+			const message = "Migration to version 0.1.17 successful, goats were herded."
 			ui.notifications.info(message);
 			console.log(message);
 			game.settings.set("prime", "notAutoIncrementedBeforeICanCheckItWorldVersionNumber", game.system.data.version);
