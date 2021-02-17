f541d9842a1bac0de2775544e39c8237f02222b8
Johnathan Castle <castle.johnathan@googlemail.com>
2021-02-17 17:31:57 +0000

    Updated version number and rezipped.

--- module/migrations/PrimeMigration_0_1_10_to_0_2_2.js
+++ module/migrations/PrimeMigration_0_1_10_to_0_2_2.js
@@ -1,20 +1,20 @@
-export class PrimeMigration_0_1_10_to_0_2_1
+export class PrimeMigration_0_1_10_to_0_2_2
 {
 	static async update()
 	{
-		ui.notifications.info("Migrating world from 0.1.10+ to version 0.2.1.");
+		ui.notifications.info("Migrating world from 0.1.10+ to version 0.2.2.");
 		var success = await this.updateItems()
 
 		if (success)
 		{
-			const message = "Migration to version 0.2.1 successful, goats were herded."
+			const message = "Migration to version 0.2.2 successful, goats were herded."
 			ui.notifications.info(message);
 			console.log(message);
 			game.settings.set("prime", "notAutoIncrementedBeforeICanCheckItWorldVersionNumber", game.system.data.version);
 		}
 		else
 		{
-			const error = "Migration to version 0.2.1 failed, goats were not herded. See console for more details.";
+			const error = "Migration to version 0.2.2 failed, goats were not herded. See console for more details.";
 			ui.notifications.error(error);
 			console.error(error);
 		}
