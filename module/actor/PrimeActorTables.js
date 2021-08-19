4ff2f2c9742d2ad6d71b7971801d93458952dcdd
jackmoxley <jackokmoxley@gmail.com>
2021-08-19 01:24:03 +0100

    Wired in item names into the drop downs.

--- module/actor/PrimeActorTables.js
+++ module/actor/PrimeActorTables.js
@@ -1,23 +1,47 @@
 
 import PrimeTables from "../PrimeTables.js";
-import {loadBasicData, TemplateTable} from "../util/TemplateTable.js";
+import {loadBasicData, loadComplexData, TemplateTable} from "../util/TemplateTable.js";
+import {arrayIfNot} from "../util/support.js";
+import {PrimeItemManager} from "../item/PrimeItemManager.js";
 
 class ActorTable extends TemplateTable {
 	_mentalConditions;
+	_woundConditions;
+
 	constructor() {
 		super('actor')
 	}
+
+	itemRefresh() {
+		this._mentalConditions = null;
+		this._woundConditions = null;
+	}
+
 	get woundConditions() {
 		if (this._woundConditions == null) {
-			this._woundConditions = loadBasicData(this.data.woundConditions);
+			this._woundConditions = ActorTable.loadInjuriesForType('wound');
 		}
 		return this._woundConditions;
 	}
+
 	get mentalConditions() {
 		if (this._mentalConditions == null) {
-			this._mentalConditions = loadBasicData(this.data.mentalConditions);
+			this._mentalConditions = ActorTable.loadInjuriesForType('insanity');
 		}
 		return this._mentalConditions;
+	}
+
+	static loadInjuriesForType(injuryType) {
+		const transformed = {}
+		const filtersData = {injuryType};
+		const criteria = {itemBaseTypes:'injury', typed: true, sortItems: true, filtersData};
+		const items = PrimeItemManager.getItems(criteria);
+		items.forEach(item => {
+			const key = item.id; // this will become sourceKey for embedded items.
+			const title = game.i18n.localize(item.name);
+			transformed[key] = {title};
+		});
+		return transformed;
 	}
 }
 
