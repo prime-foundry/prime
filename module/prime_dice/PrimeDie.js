a25dc1775fed522d1728d62050a24bfe554fd83d
CYBER\jc-work <castle.johnathan@googlemail.com>
2023-06-01 20:12:44 +0100

    Whitespace

--- module/prime_dice/PrimeDie.js
+++ module/prime_dice/PrimeDie.js
@@ -1,14 +1,14 @@
 
 const PRIME_TABLE = [-5, -4, -3, -2, -2, -1, -1, -1, 0, 0, 0, 0, 1, 1, 1, 2, 2, 3, 4, 5];
-export default class PrimeDie extends DiceTerm 
+export default class PrimeDie extends DiceTerm
 {
-    constructor(termData) 
+    constructor(termData)
     {
         super(termData);
         this.faces = 20;
     }
 
-    _roll() 
+    _roll()
     {
         const rand = CONFIG.Dice.randomUniform();
         const result = Math.ceil(rand * this.faces);
@@ -19,10 +19,10 @@ export default class PrimeDie extends DiceTerm
     }
 
     /** @override */
-    get total() 
+    get total()
     {
         if ( !this._evaluated ) return null;
-        return this.results.reduce((t, r) => 
+        return this.results.reduce((t, r) =>
         {
             if ( !r.active ) return t;
             else return t + r.primeModifier;
@@ -30,18 +30,18 @@ export default class PrimeDie extends DiceTerm
     }
 
     /** @override */
-    roll() 
+    roll()
     {
         let roll = this._roll();
         let checked = 0;
-        while ( checked < this.results.length ) 
+        while ( checked < this.results.length )
         {
             let r = this.results[checked];
             checked++;
             if (!r.active) continue;
 
             // Determine whether to explode the result and roll again!
-            if ( r.result === 1 || r.result === 20) 
+            if ( r.result === 1 || r.result === 20)
             {
                 r.exploded = true;
                 roll = this._roll();
